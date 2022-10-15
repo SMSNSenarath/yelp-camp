23d1009b3597f98df04c59afe20bc2e76f08af7d
Shakila Nishadini <smsnshakilaa372@gmail.com>
2022-10-15 21:00:09 +0530

    Did changes in uploading images in Create and Edit campgrounds

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -2,6 +2,8 @@
       (function () {
         "use strict";
 
+        bsCustomFileInput.init();
+
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         const forms = document.querySelectorAll(".validated-form");
 
